affc00a280c21bbffdcc3aafe2b29338445c8815
stgary <sgary0@protonmail.com>
2020-10-13 18:10:00 -0400

    init about jsx and styling

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Profile from '../img/pic42.png'
+
+export default function About() {
+  return (
+    <div className='about'>
+      <div className='about-header'>
+        About
+      </div>
+      <div className='about-container'>
+        <img className='about-img' src={Profile} alt='profile for stephen gary' />
+        <div className='about-para'>
+          <p> 
+            Full stack developer who's passionate about creating intuitive 
+            simple solutions to complex problems. I've worked in multiple 
+            industries with distinct roles and had a great amount of success.
+          </p>
+          <p>
+            I'm a tireless seeker of knowledge, an occasional purveyor 
+            of wisdom and as luck would have it I'm available for hire.    
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
