c33a1c4580d995757df554636075b944eef4fdc9
stgary <sgary0@protonmail.com>
2020-10-13 18:15:57 -0400

    init skills jsx and styling

---
+++ src/components/Skills.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+    <div className='skills'>
+      <div className='skills-header'>
+        Skills
+      </div>
+      <div className='skills-container'>
+        <div className='skills-item'>
+          <p>Node.js</p>
+          <p>Express</p>
+          <p>PostgreSql</p>
+        </div>
+        <div className='skills-item'>
+          <p>JavaScript</p>
+          <p>HTML</p>
+          <p>Python</p>
+        </div>
+        <div className='skills-item'>
+          <p>React</p>
+          <p>Redux</p>
+          <p>Less/Sass</p>
+        </div>
+        <div className='skills-item'>
+          <p>CSS</p>
+          <p>Sqlite3</p>
+          <p>MongoDB</p>
+        </div>
+      </div>
+    </div>
+  )
+}
