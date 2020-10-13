9f24218749227ef4ef3cb0c92a67446ea8e161e4
stgary <sgary0@protonmail.com>
2020-10-13 16:44:17 -0400

    jsx for header complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from './Header';
+
 export default function App() {
   return (
-    <div className="App">
-
+    <div className='top'>
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faLinkedinIn,
+  faFacebook,
+  faTwitter,
+  faGithub 
+} from '@fortawesome/free-brands-svg-icons';
 
 export default function Header() {
   return (
-    <div>
-      
+    <div className='header'>
+      <p className='header-name'>Stephen Gary</p>
+      <p className='header-title'>Web Developer</p>
+      <div className='icon-container'>
+        <a href='https://github.com/stgary'>
+          <FontAwesomeIcon className='icon' icon={faGithub} />
+        </a>
+        <a href='https://www.linkedin.com/in/sgary0'>
+          <FontAwesomeIcon className='icon' icon={faLinkedinIn} />
+        </a>
+        <a href='https://twitter.com/explore'>
+          <FontAwesomeIcon className='icon' icon={faTwitter} />
+        </a>
+        <a href='https://m.facebook.com/stephen.gary.566'> 
+          <FontAwesomeIcon className='icon' icon={faFacebook} />
+        </a> 
+      </div>
     </div>
   )
 }
