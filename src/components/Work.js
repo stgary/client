d2951cdb63a5fad30f329d4a8ba9e4a13dd220f2
stgary <sgary0@protonmail.com>
2020-11-01 19:32:00 -0500

    styles for headers and buttons

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,60 +10,62 @@ export default function Work() {
         <div className='work-group'>
           <div className='work-item'>
             <p className='year'>2017-2019</p>
-            <p className='company'>Clemens Food Group</p>
             <p className='position'>Electronics Tech</p>
+            <p className='company'>Clemens Food Group</p>
           </div>
           <div className='work-para'>
-            Clemens is the fifth largest pork producer in the world. My job consisted of electrical
-            maintenance, PLC programming, device calibration, and project work.
+            Work environment: Siemens Simatic 7 HMI's and PLC's. Siemens VFD. Massman palletizing. My duties 
+            consisted of device calibration, installation, and maintenance.
           </div>
         </div>
         
         <div className='work-group'>
           <div className='work-item'>
             <p className='year'>2014-2017</p>
-            <p className='company'>Vijon laboratories</p>
             <p className='position'>PLC Programmer</p>
+            <p className='company'>Vijon laboratories</p>
           </div>
           <div className='work-para'>
-            Vijon is a major manufacturer of cleaning and cosmetic products. 
-            My job consisted of electrical maintenance, PLC programming, integration, and installation.
+            Work environment: Allen Bradley HMI's, PLC5, SLC500, Micrologix, Controllogix. 
+            Rxlogix500, 5000, and studio5000. Yaskawa and Powerfelx VFD, Baldor and SEW motors,
+            Massman palletizing and LanFranchi bottling systems. My duties consisted of PLC programming,
+            device integration and electrical maintenance.    
           </div>
         </div>
 
         <div className='work-group'>
           <div className='work-item'>
             <p className='year'>2013-2014</p>
-            <p className='company'>Benteler Automotive</p>
             <p className='position'>Robotic Programmer</p>
+            <p className='company'>Benteler Automotive</p>
           </div>
           <div className='work-para'>
-            Benteler is an automotive parts manufacturer. My job consisted of 
-            electrical maintenance and robotic programming.
+            Work environment: Siemens Simatic 7 HMI's and PLC's. Fanuc, Motoman and ABB Robotics. Miller, Lincoln, and WTC weld units. My duties 
+            consisted of Robotic programming and maintenance.
           </div>
         </div>
 
         <div className='work-group'>
           <div className='work-item'>
             <p className='year'>2012-2013</p>
-            <p className='company'>Traumasoft</p>
             <p className='position'>Web Developer</p>
+            <p className='company'>Traumasoft</p>
           </div>
           <div className='work-para'>
-            Traumasoft Built a call intake system for ambulances and web applications. 
-            My job consisted of using HTML, CSS, and JavaScript for the design and modification of web applications.
+            Work environment: EmberJS, BackboneJS, mustacheJS, HTML5, CSS3 and PHP. My duties consisted of design 
+            and modification of web applications.
           </div>
         </div> 
 
         <div className='work-group'>   
           <div className='work-item'>
             <p className='year'>2011-2012</p>
-            <p className='company'>Challenge MFG</p>
             <p className='position'>Robotic Programmer</p>
+            <p className='company'>Challenge MFG</p>
           </div>
           <div className='work-para'>
-            Challenge Mfg. is an automotive parts manufacturer. 
-            My job consisted of electrical maintenance and robotic programming.
+            Work environment: Allen Bradley HMI's, Controllogix and Micrologix. Fanuc, Motoman and ABB Robotics. Lincoln and WTC welding units. My duties 
+            consisted of Robotic programming and maintenance.
           </div>
         </div>
       </div>
