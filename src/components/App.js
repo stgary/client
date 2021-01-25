fb74a465866e139c4c1a13fa1e29bc212c6c1478
stgary <sgary0@protonmail.com>
2021-01-25 18:08:24 -0500

    working on counter for page veiws

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 import Header from './Header';
 import About from './About';
@@ -10,6 +11,20 @@ import Contact from './Contact';
 import Footer from './Footer';
 
 export default function App() {
+
+  const END_POINT = process.env.END_POINT || 'https://resume-bkend.herokuapp.com/count';
+
+  useEffect(() => {
+    visits();
+  })
+
+  const visits = () => {
+    axios 
+      .put(END_POINT, { views: 1 })
+      .then(res => 'Success')
+      .catch(err => 'Failure')
+  }
+
   return (
     <div className='top'>
       <Header />
